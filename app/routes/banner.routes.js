174eb951ecb4335be57665734a956c0ab3c59db1
pqthinh <phamquangquang2008@gmail.com>
2020-11-06 21:35:03 +0700

    banner err

--- app/routes/banner.routes.js
+++ app/routes/banner.routes.js
@@ -1,4 +1,4 @@
 module.exports = app => {
-    const banner = require("../controllers/banner.controllers.js");
-    app.get("/banner", banner.findAll);
-  };
+  const banner = require("../controllers/banner.controllers.js");
+  app.get("/banner", banner.findAll);
+};
