d8eff6c77d73fd0243079a8e79404b6d3a1b821c
Huy Doan <doanvanhuy0907@gmail.com>
2020-12-07 22:42:07 +0700

    file upload

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
 const path = require('path');
 const app = express();
 
@@ -8,10 +9,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
 app.use("/data", express.static("data"));
-
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to my app" });
-});
  
 require("./app/routes/news.routes.js")(app);
 require("./app/routes/banner.routes.js")(app);
