1df7e8357c4fec2b151b4b0ea45554090a3867db
pqthinh <50290559+pqthinh@users.noreply.github.com>
2020-12-10 10:26:31 +0700

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,15 +1,15 @@
-const db = {
-    HOST: "remotemysql.com",
-    USER: "ZduVtEX3Nd",
-    PASSWORD: "bleVGxRMN2",
-    DB: "ZduVtEX3Nd"
-}
-// database localhost
 // const db = {
-//     HOST: "127.0.0.1",
-//     USER: "root",
-//     PASSWORD : "",
-//     DB: "chotot"
+//     HOST: "remotemysql.com",
+//     USER: "ZduVtEX3Nd",
+//     PASSWORD: "bleVGxRMN2",
+//     DB: "ZduVtEX3Nd"
 // }
+// database localhost
+const db = {
+    HOST: "127.0.0.1",
+    USER: "root",
+    PASSWORD : "",
+    DB: "chotot"
+}
 
 module.exports = db
